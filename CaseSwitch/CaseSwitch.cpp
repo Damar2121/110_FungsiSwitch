62d53b4920537058beb933b30611847d61625fa7
Damar2121 <damar.vivoy51@gmail.com>
2024-03-25 10:18:15 +0700

    fungsi do

--- CaseSwitch/CaseSwitch.cpp
+++ CaseSwitch/CaseSwitch.cpp
@@ -28,4 +28,21 @@ int pembagian(int bil_1, int bil_2) {
 	return bil_1 / bil_2;
 }
 
+int main()
+{
+	int pilihan;
+
+	do {
+		system("cls");
+		cout << "=============== menu ===============" << endl;
+		cout << "1. penjumlahan" << endl;
+		cout << "2. pengurangan" << endl;
+		cout << "3. perkalian" << endl;
+		cout << "4. pembagian" << endl;
+		cout << "5. keluar" << endl;
+		cout << "masukkan menu yang ingin di pilih (1/2/3/4/5)";
+		cin >> pilihan;
+	}
+}
+
 
